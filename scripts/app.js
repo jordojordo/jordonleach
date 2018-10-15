2942f3e640a5fd36e3c46ac2caf9d0b097de1cfd
Jordon Leach <40806497+jordonleach@users.noreply.github.com>
2018-10-15 15:48:01 -0400

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,112 @@
+particlesJS('particles-js',
+	{
+	  "particles": {
+	    "number": {
+	      "value": 80,
+	      "density": {
+	        "enable": true,
+	        "value_area": 800
+	      }
+	    },
+	    "color": {
+	      "value": "#ffffff"
+	    },
+	    "shape": {
+	      "type": "circle",
+	      "stroke": {
+	        "width": 3,
+	        "color": "#f0caca"
+	      },
+	      "polygon": {
+	        "nb_sides": 5
+	      },
+	      "image": {
+	        "src": "img/github.svg",
+	        "width": 100,
+	        "height": 100
+	      }
+	    },
+	    "opacity": {
+	      "value": 0,
+	      "random": false,
+	      "anim": {
+	        "enable": false,
+	        "speed": 0,
+	        "opacity_min": 0,
+	        "sync": false
+	      }
+	    },
+	    "size": {
+	      "value": 7,
+	      "random": true,
+	      "anim": {
+	        "enable": true,
+	        "speed": 10,
+	        "size_min": 7.192347342427655,
+	        "sync": false
+	      }
+	    },
+	    "line_linked": {
+	      "enable": true,
+	      "distance": 236.7480290726578,
+	      "color": "#FFDAC1",
+	      "opacity": 0.4,
+	      "width": 1
+	    },
+	    "move": {
+	      "enable": true,
+	      "speed": 2,
+	      "direction": "none",
+	      "random": true,
+	      "straight": false,
+	      "out_mode": "out",
+	      "bounce": false,
+	      "attract": {
+	        "enable": true,
+	        "rotateX": 600,
+	        "rotateY": 1200
+	      }
+	    }
+	  },
+	  "interactivity": {
+	    "detect_on": "window",
+	    "events": {
+	      "onhover": {
+	        "enable": true,
+	        "mode": "repulse"
+	      },
+	      "onclick": {
+	        "enable": true,
+	        "mode": "push"
+	      },
+	      "resize": true
+	    },
+	    "modes": {
+	      "grab": {
+	        "distance": 400,
+	        "line_linked": {
+	          "opacity": 1
+	        }
+	      },
+	      "bubble": {
+	        "distance": 200,
+	        "size": 20,
+	        "duration": 1,
+	        "opacity": 3,
+	        "speed": 3
+	      },
+	      "repulse": {
+	        "distance": 80,
+	        "duration": 0.4
+	      },
+	      "push": {
+	        "particles_nb": 4
+	      },
+	      "remove": {
+	        "particles_nb": 2
+	      }
+	    }
+	  },
+	  "retina_detect": true
+	}
+)
